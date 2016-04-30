6551ac5e66b2594fc2630b7f83b836297f0eb9d3
David Yang <daworld.ny@gmail.com>
2016-04-30 22:38:13 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -3,7 +3,7 @@
 return array(
     'module' => array(
         'name'  => 'Stokes',
-        'url'   => '/admin/module/stokes',
+        'route' => 'Stokes:Admin:Stoke@gridAction',
         'icon'  => 'fa fa-money fa-5x',
         'order' => 1,
         'description' => 'Stoke module allows you to add random stuff that has data of expiration',
