4fde11e1adf4f0f159b5fcdd731570c49a835724
David Yang <daworld.ny@gmail.com>
2016-08-01 22:29:48 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name'  => 'Stokes',
-        'route' => 'Stokes:Admin:Stoke@gridAction',
-        'icon'  => 'fa fa-money fa-5x',
-        'order' => 1,
-        'description' => 'Stoke module allows you to add random stuff that has data of expiration',
-    )
+ 'name'  => 'Stokes',
+ 'route' => 'Stokes:Admin:Stoke@gridAction',
+ 'icon'  => 'fa fa-money fa-5x',
+ 'order' => 1,
+ 'description' => 'Stoke module allows you to add random stuff that has data of expiration',
 );
